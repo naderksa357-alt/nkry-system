b606d0c152321f3d798c2329ff8ac349740c02cd
naderksa357-alt <naderksa357@gmail.com>
2026-02-28 01:57:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "NKRY system is running"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
