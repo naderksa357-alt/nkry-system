ad3f4b1ce28510e53897edbe4280f82b6a0a563d
naderksa357-alt <naderksa357@gmail.com>
2026-03-01 10:19:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI, Form
 import os
 
+# إنشاء التطبيق
 app = FastAPI()
 
-
+# الصفحة الرئيسية
 @app.get("/")
 def home():
     return {"message": "NKRY system is running"}
 
-
+# حفظ طلب
 @app.post("/submit-order")
 def submit_order(name: str = Form(...), phone: str = Form(...)):
     return {
@@ -17,9 +18,10 @@ def submit_order(name: str = Form(...), phone: str = Form(...)):
         "phone": phone
     }
 
-
+# تشغيل السيرفر
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
+    
     
